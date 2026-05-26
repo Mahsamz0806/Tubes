59cfba48591b915aab296e0c20516769c0ff2a3e
Mahsamz0806 <mahsazahran08@gmail.com>
2026-05-26 16:39:36 +0700

    initial commit Tubes Alpro 2

---
+++ ClearMind.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var inputan string
+	fmt.Println("=== Halo Selamat Datang Di ClearMind Chatbot ===\n")
+	fmt.Println("Bagaimana Harimu berjalan?")
+	fmt.Println("(sangat_baik/baik/kurang_baik/buruk)")
+	fmt.Scan(&inputan)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Tubes_Alpro2
+
+go 1.25.0
